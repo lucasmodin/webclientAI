b5301d6b9921c8154d8370268d8738ec5d581be4
lucasmodin <lumo0002@stud.kea.dk>
2025-04-21 23:15:21 +0200

    changed to gpt model and trying changes to system prompt to see if the model would better know context

--- src/main/java/webclient/webclientai/service/MistralService.java
+++ src/main/java/webclient/webclientai/service/MistralService.java
@@ -18,14 +18,15 @@ public class MistralService {
 
     @Autowired
     public MistralService (WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://api.mistral.ai/v1/chat/completions").build();
+        this.webClient = webClientBuilder.baseUrl("https://api.openai.com/v1/chat/completions").build();
+
     }
 
     @Value("${openai.api.key}")
     private String openapikey;
     public Map<String,Object> promptMistral() {
         RequestDTO requestDTO = new RequestDTO();
-        requestDTO.setModel("mistral-small-latest");
+        requestDTO.setModel("gpt-4o-mini");
         requestDTO.setTemperature(1.0);
         List<Message> lstMessages = new ArrayList<>(); //en liste af messages med roller
         lstMessages.add(new Message("system", "You are a helpful assistant and an expert in World of Warcraft (WoW)."));
@@ -55,7 +56,11 @@ public class MistralService {
             String userInput,
             PromptContext context
     ) {
-        String systemPrompt = "You are a helpful assistant and an expert WoW PvE coach.";
+        String systemPrompt = "You are a World of Warcraft PvE expert with up-to-date knowledge of Mythic+ content for The War Within – Season 2." +
+                              " This week's dungeon pool: Operation: Floodgate, Cinderbrew Meadery, Darkflame Cleft, The Rookery, Priory of the Sacred Flame, The MOTHERLODE!!, Theater of Pain, Operation: Mechagon – Workshop." +
+                              " Active affixes (April 16–22, 2025): Xal’atath’s Bargain: Devour – applies shields that must be healed or dispelled to avoid enemy healing; removal grants +2% max HP and +4% Crit for 30s. " +
+                              "Tyrannical – bosses have +30% health and +15% damage. " +
+                              "Fortified – non-boss mobs have +20% health and +30% damage. Use this context to provide expert-level advice and analysis.\n";
         String combinedPrompt = PromptBuilder.buildPromptWithContext(context, character, gear, userInput);
 
         //debugging
@@ -64,7 +69,7 @@ public class MistralService {
         System.out.println("===========================");
 
         RequestDTO requestDTO = new RequestDTO();
-        requestDTO.setModel("mistral-small-latest");
+        requestDTO.setModel("gpt-4o-mini");
         requestDTO.setTemperature(1.0);
 
 
